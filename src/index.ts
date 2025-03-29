bcc3be2acf3a5db6f81bcccb750e7d344a737266
Rue Juniper Grant <nataly.arsenieva@gmail.com>
2025-03-29 19:21:10 -0400

    Checking messages

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express';
+import { VK } from 'vk-io';
 const express = require('express');
 const app = express();
 app.use(express.json());
+
+const vk = new VK({
+  token: 'vk1.a.Aa8fXGArMpnErXAZU-plD2BgXEPWktN7wukiRbLejwR11DxeuEPshJPv0Agi1uBMzyu-GyNO6kmx6xpEedk7CSiMHflwBubk5xf13IlOcAJNBzt49UL_5ecZrDPHn2ELK_cQtZROAhyatYlu9ItFajrfzSGlfLVZkaggYCee0slTapMNrww2894nPug1xIyq5FFRCB20aoPhRm-C3ETyfw'
+});
 
 // Токены
 const confirmationToken = '7c112053';
@@ -11,24 +16,32 @@ app.listen(PORT, () => {
   console.log(`Сервер запущен на порту ${PORT}`);
 });
 
-// Обработка входящих запросов
 app.post('/', (req: Request, res: Response) => {
-  console.log(req.body);
   const data = req.body;
-
   if (!data || !data.type) {
-    // Если нет "type", просто завершаем
     return res.end();
   }
 
   switch (data.type) {
     case 'confirmation':
-      // Возвращаем строку для подтверждения (confirmationToken)
       return res.send(confirmationToken);
     default:
-      // Если пришёл неизвестный тип, отвечаем чем-то формальным
       return res.end('ok');
   }
 });
 
+// Подписываемся на событие "новое сообщение"
+vk.updates.on('message_new', async (context, next) => {
+  // Если это не системное сообщение, а именно пользовательское
+  if (context.isUser && context.text) {
+    // Например, если пользователь написал "Привет"
+    if (context.text.toLowerCase() === 'a') {
+      // Отправляем ответ в чат/ЛС
+      await context.send('хуй на');
+    }
+  }
+});
+
+// Запускаем Long Poll
+vk.updates.start().catch(console.error);
 
