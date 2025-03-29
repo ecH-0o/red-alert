e9ccd2a2c6793db94317e0ab4ffb5c8551d4830e
Rue Juniper Grant <nataly.arsenieva@gmail.com>
2025-03-29 19:40:53 -0400

    zzzzzzzzzzzz

--- src/index.ts
+++ src/index.ts
@@ -8,35 +8,39 @@ const vk = new VK({
   token: 'vk1.a.Aa8fXGArMpnErXAZU-plD2BgXEPWktN7wukiRbLejwR11DxeuEPshJPv0Agi1uBMzyu-GyNO6kmx6xpEedk7CSiMHflwBubk5xf13IlOcAJNBzt49UL_5ecZrDPHn2ELK_cQtZROAhyatYlu9ItFajrfzSGlfLVZkaggYCee0slTapMNrww2894nPug1xIyq5FFRCB20aoPhRm-C3ETyfw'
 });
 
-// Токены
 const confirmationToken = '7c112053';
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Сервер запущен на порту ${PORT}`);
-});
-
 app.post('/', async (req: Request, res: Response) => {
-  const data = req.body;
-  if (!data || !data.type) {
-    return res.end();
+  console.log('Request received:', req.body);
+  const { type, object } = req.body;
+  if (!type) return res.end();
+
+  if (type === 'confirmation') {
+    console.log('Confirmation request');
+    return res.send(confirmationToken);
   }
 
-  switch (data.type) {
-    case 'confirmation':
-      return res.send(confirmationToken);
-    case 'message_new':
-      const text = data.object?.message?.text?.toLowerCase();
-      if (text === 'a') {
+  if (type === 'message_new') {
+    console.log('New message event:', object);
+    const text = object?.message?.text?.toLowerCase();
+    if (text === 'a') {
+      try {
         await vk.api.messages.send({
-          peer_id: data.object.message.peer_id,
+          peer_id: object.message.peer_id,
           message: 'Ответ по Callback API',
           random_id: Date.now()
         });
+        console.log('Message sent successfully.');
+      } catch (error) {
+        console.error('Error sending message:', error);
       }
-      return res.end('ok');
-    default:
-      return res.end('ok');
+    }
+    return res.end('ok');
   }
+  return res.end('ok');
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
