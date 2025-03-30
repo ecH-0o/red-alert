e1ed98a6e96a292932b9e0d9645a5161996bb2c5
Rue Juniper Grant <nataly.arsenieva@gmail.com>
2025-03-29 20:37:53 -0400

    Trying FB

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,24 @@
 import express from 'express';
 import { VK } from 'vk-io';
+import admin from 'firebase-admin';
+import serviceAccount from '../serviceAccountKey.json';
+import 'dotenv/config';
 
+
+// Инициализируем Firebase Admin SDK
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
+  databaseURL: 'https://red-alert-1748e.firebaseio.com'  // замените на URL вашей базы
+});
 const app = express();
+
+// Если используете Firestore:
+const db = admin.firestore();
 
 // Инициализируйте vk-io с токеном сообщества.
 // Рекомендуется хранить токен в переменной окружения (VK_TOKEN)
 const vk = new VK({
-  token: process.env.VK_TOKEN || 'vk1.a.I9jMXprw-Y2n9jD1QrtPG5-OT3kblogrwDOpIkRNgPV3BIJBvn83ocWePJIhMEJeFnepm4qnFePrgJKouzCsbLZ4PYtJwBad9pRZuRRlj6kZ0alUhEcDKx3lZgqEuVnm0urJiQMOsmegTFnvmudv4_u2fPPiKYkby3g1MMyKJX3bQGsg_e2PSIMmOVLc2TJRvUnikKBMtsRtmnqVqohBdA'
+  token: process.env.VK_TOKEN || 'placeholder'
 });
 
 // Обработчик новых сообщений через Long Poll API
@@ -15,8 +27,19 @@ vk.updates.on('message_new', async (context) => {
 
   // Если текст равен 'a' (без учета регистра), отправляем ответ
   if (context.text && context.text.toLowerCase() === 'a') {
+     // Запрос данных из коллекции (замените 'your_collection_name' на имя вашей коллекции)
+     const snapshot = await db.collection('users').get();
+      
+     let result = '';
+     snapshot.forEach(doc => {
+       result += `${doc.data}`;
+     });
+
+     if (!result) {
+       result = 'База пуста.';
+     }
     try {
-      await context.send('Ответ от Long Poll API в беседе!');
+      await context.send(result);
       console.log(`Ответ отправлен в чат с peer_id ${context.peerId}`);
     } catch (error) {
       console.error('Ошибка при отправке сообщения:', error);
