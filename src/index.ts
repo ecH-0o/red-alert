c0dc28948ee4a30d092ecf4dc8dc6ed0d7238353
Rue Juniper Grant <nataly.arsenieva@gmail.com>
2025-03-29 22:17:06 -0400

    real testing rn

--- src/index.ts
+++ src/index.ts
@@ -32,19 +32,11 @@ const keyboard = {
       {
         action: {
           type: 'text',
-          payload: JSON.stringify({ button: "1" }),
-          label: 'Кнопка 1'
+          payload: JSON.stringify({ action: 'show_details' }),
+          label: 'нажать'
         },
         color: 'primary'
       },
-      {
-        action: {
-          type: 'text',
-          payload: JSON.stringify({ button: "2" }),
-          label: 'Кнопка 2'
-        },
-        color: 'positive'
-      }
     ]
   ]
 };
@@ -53,19 +45,32 @@ const keyboard = {
 // Обработчик новых сообщений через Long Poll API
 vk.updates.on('message_new', async (context) => {
   console.log(`Получено сообщение от peer_id ${context.peerId}: ${context.text}`);
-
+  const payload = context.messagePayload; // или context.getMessagePayload()
   // Если текст равен 'a' (без учета регистра), отправляем ответ
-  if (context.text && context.text.startsWith('[id')) {
+  if (context.text && context.text.startsWith('ф')) {
     
     try {
       await context.send({
-        message: 'Выберите кнопку:',
+        message: 'жми на рычаг',
         keyboard: JSON.stringify(keyboard)
       });
       console.log(`Ответ отправлен в чат с peer_id ${context.peerId}`);
     } catch (error) {
       console.error('Ошибка при отправке сообщения:', error);
     }
+  }
+  // Если пришло сообщение с payload (нажатие на кнопку)
+  if (payload && payload.action === 'show_details') {
+    let message = ''
+    await db.collection('users').where("vkID", "==", context.senderId).get().then(async (user: any) => {
+      if (user) {
+        message+= `Участник: [id${context.senderId}|${user[0].name}]`;
+        message+= `\n\nРоль: ${user[0].role}ㅤ★ㅤ${user[0].fandom.name}]`;
+        message+= `\n\nТекущая локация: ${user[0].current_location.location} в регионе ${user[0].current_location.globalLocation.name}]`;
+      }
+    });
+    await context.send(message);
+    return;
   }
 });
 
